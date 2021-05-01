7ae597a4d1d55f9aa44fa0b6f1f00919e2b33ffb
flyhigh <fly325m@gmail.com>
2021-05-01 09:52:40 +0900

    자바버전 10 var 변수 사용

---
+++ src/ch09/LocalVariableType.java
@@ -0,0 +1,17 @@
+package ch09;
+
+public class LocalVariableType {
+
+    public static void main(String[] args) {
+
+        var i = 10;
+        var j = 10.0;
+        var str = "hello";
+
+        System.err.println(i);
+        System.err.println(j);
+        System.err.println(str);
+
+    }
+
+}
